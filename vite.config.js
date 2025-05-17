78b86d8793c01da1809c027962434e450e462940
theforcebe <keanenmaas@gmail.com>
2025-05-16 18:13:18 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/MOP-Classic/', // ← Use your repo name here
   plugins: [react()],
 })
